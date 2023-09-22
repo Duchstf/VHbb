1225d633f75e27919af22268dabdff2ab98b4e5f
Duchstf <minhduc8199@gmail.com>
2023-09-22 03:14:45 -0500

    back up studies

--- output/scripts/make_pkl.py
+++ output/scripts/make_pkl.py
@@ -31,7 +31,7 @@ def main():
 
     year = sys.argv[1]
     tag = sys.argv[2]
-    hist_name = 'ParticleNet_msd' #You need to define this manually
+    hist_name = 'templates' #You need to define this manually
             
     indir = "../coffea/{}/{}/".format(tag, year)
     infiles = subprocess.getoutput("ls "+indir+year+"_dask_*.coffea").split()
