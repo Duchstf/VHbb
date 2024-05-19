d9f5f3cffa2dc5b28fc54783a52d3f9260acbfd2
Duchstf <minhduc8199@gmail.com>
2024-05-19 18:34:25 -0500

    back up codes

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,5 +1,6 @@
 #Try submitting stuff twice
-python submit/submit-official.py 2016APV > dask.out 2>&1
-python submit/submit-official.py 2016APV > dask.out 2>&1
-python submit/submit-official.py 2016APV > dask.out 2>&1
-python submit/submit-official.py 2016APV > dask.out 2>&1
+python submit/submit-official.py 2017 > dask.out 2>&1
+python submit/submit-official.py 2017 > dask.out 2>&1
+python submit/submit-official.py 2017 > dask.out 2>&1
+python submit/submit-official.py 2017 > dask.out 2>&1
+python submit/submit-official.py 2017 > dask.out 2>&1
