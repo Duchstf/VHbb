9fe9152e1f2539084bb4aa82ddc8a5181bc97eda
Duc Hoang <minhduc8199@gmail.com>
2024-06-02 03:54:50 +0200

    2016 Impacts AN V1

--- combine/combine_official/year_scripts/plot_impacts.sh
+++ combine/combine_official/year_scripts/plot_impacts.sh
@@ -15,6 +15,7 @@ fi
 #Run impacts_fit first
 #Add -spool to condor submit command, like this: condor_submit -spool condor_run_impacts.sub
 #Then run these interactively
+modelfile=output/testModel${year}/model_combined.root
 
 combineTool.py -M Impacts -d $modelfile -m 125 -o impacts.json --exclude 'rgx{qcdparams*}' -t -1 --redefineSignalPOIs rVH  
 plotImpacts.py -i impacts.json -o impacts
