1dbfa7ed6734c67ad6e26b1001ffaf1b1b9e8ade
Duchstf <minhduc8199@gmail.com>
2024-07-08 09:33:16 -0500

    back up codes

--- boostedhiggs/__init__.py
+++ boostedhiggs/__init__.py
@@ -6,6 +6,7 @@ from .vhbb_WtagCR import VHBB_WTagCR
 from .vhbb_PNQCD_Scan import VhbbPNQCDScan
 from .vhbb_ddt_map import DDT
 from .vhbb_official_v11 import VHbbProcessorV11
+from .vhbb_2D_mass import VHbbProcessor2DMass
 
 __all__ = [
     '__version__',
