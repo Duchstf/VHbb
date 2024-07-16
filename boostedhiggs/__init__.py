4f7944cef62256ad09d838329c378feab9223bb8
Duchstf <minhduc8199@gmail.com>
2024-07-16 08:55:34 -0500

    back up

--- boostedhiggs/__init__.py
+++ boostedhiggs/__init__.py
@@ -3,9 +3,7 @@ from .vhbb_OFFICIAL import VHbbProcessorOfficial
 from .vhbb_OFFICIAL_TheorySys import VHbbProcessorOfficial_TheorySys
 from .vhbb_muonCR import VHBB_MuonCR_Processor
 from .vhbb_WtagCR import VHBB_WTagCR
-from .vhbb_PNQCD_Scan import VhbbPNQCDScan
 from .vhbb_ddt_map import DDT
-from .vhbb_official_v11 import VHbbProcessorV11
 from .vhbb_2D_mass import VHbbProcessor2DMass
 from .vhbb_HEM import VHbbProcessorHEM
 
