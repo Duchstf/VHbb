25f047efa31b2c5e2c0bab107d14d5fae28f7a19
Duchstf <minhduc8199@gmail.com>
2024-07-17 10:53:33 -0500

    modify the muondata redirector

--- datasets/make_infiles.py
+++ datasets/make_infiles.py
@@ -7,6 +7,7 @@ python make_infiles.py
 """
 
 custom_list = ['VBFDipoleRecoilOn']
+target_list = ['muondata']
 
 import os
 import json
@@ -14,14 +15,15 @@ import subprocess, sys
 
 with open('../files/pmap.json', 'r') as f: pmap = json.load(f)
 
-for year in ['2016', '2016APV', '2017', '2018']:
+for year in ['2016', '2016APV' ,'2017', '2018']:
 
     with open(f'nano_list/datasets_{year}.json', 'r') as f: datasets = json.load(f)
 
     for k, val in pmap.items():
 
         # one infile per pmap key
-        if k in custom_list: continue
+        # if k in custom_list: continue
+        if k not in target_list: continue
 
         outfilename = f'infiles/{year}/{year}_{k}.json'
         filesets = {}
@@ -38,8 +40,9 @@ for year in ['2016', '2016APV', '2017', '2018']:
                 longstring = os.popen("dasgoclient --query=\"file dataset="+d+"\"").read()
                 files_no_redirector = longstring.split('\n')
 
-                if ('data' in k): filesets[v] += ["root://cmsxrootd.fnal.gov//store/test/xrootd/T1_US_FNAL" + f for f in files_no_redirector if len(f) > 0]
-                else: filesets[v] += ["root://cmsxrootd-site.fnal.gov/" + f for f in files_no_redirector if len(f) > 0]
+                # if ('data' in k): filesets[v] += ["root://cmsxrootd.fnal.gov//store/test/xrootd/T1_US_FNAL" + f for f in files_no_redirector if len(f) > 0]
+                # else: filesets[v] += ["root://cmsxrootd-site.fnal.gov/" + f for f in files_no_redirector if len(f) > 0]
+                filesets[v] += ["root://cmsxrootd-site.fnal.gov/" + f for f in files_no_redirector if len(f) > 0]
 
             print(v, len(filesets[v]))
            
