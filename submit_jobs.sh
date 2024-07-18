8a5844430360758e269366c5f613af600f2faa32
Duchstf <minhduc8199@gmail.com>
2024-07-18 03:47:51 -0500

    w tag submit jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,5 +1,5 @@
 #Try submitting stuff several times
-# ssh -L 8787:localhost:8787 dhoang@cmslpc335.fnal.gov
+# ssh -L 8787:localhost:8787 dhoang@cmslpc325.fnal.gov
 # ./submit_jobs.sh 2017 > dask.out 2>&1
 year=$1
 python submit/submit_wTagCR.py $year
