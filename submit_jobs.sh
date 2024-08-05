bd0cf231d484bd7fe1b701a8a1b0ef304e8ae3d7
Duchstf <minhduc8199@gmail.com>
2024-08-05 08:07:08 -0500

    back up 2016APV run

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,5 +1,7 @@
 #Try submitting stuff several times
 # ssh -L 8787:localhost:8787 dhoang@cmslpc342.fnal.gov
-# ./submit_jobs.sh 2016 > dask.out 2>&1
+# ./submit_jobs.sh 2016APV > dask.out 2>&1
 year=$1
-python submit/submit_official_TheorySys.py 2016 > dask.out 2>&1
+python submit/submit_official_TheorySys.py $year > dask.out 2>&1
+python submit/submit_official_TheorySys.py $year > dask.out 2>&1
+python submit/submit_official_TheorySys.py $year > dask.out 2>&1
