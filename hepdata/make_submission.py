25ec7625a41a559070e397adf88f827a12fe703c
Duchstf <minhduc8199@gmail.com>
2026-01-14 14:19:19 -0600

    back up codes

--- hepdata/make_submission.py
+++ hepdata/make_submission.py
@@ -124,20 +124,34 @@ def table_1():
     year.values = ["2016.0","2016.5","2017","2018","Combined"]
     table.add_variable(year)
 
+    # ---------------- mu_VH ----------------
     mu_VH = Variable("mu_VH", is_independent=False, is_binned=False, units="")
     mu_VH.values = [1.02, 1.34, 0.00, 1.04, 0.72]
+
+    mu_VH_up   = [2.52, 2.20, 1.26, 1.22, 0.75]
+    mu_VH_down = [2.23, 1.87, 1.18, 1.10, 0.71]
+
+    mu_VH_low  = [m - d for m, d in zip(mu_VH.values, mu_VH_down)]
+    mu_VH_high = [m + u for m, u in zip(mu_VH.values, mu_VH_up)]
+
     mu_VH_unc = Uncertainty("mu_VH uncertainty", is_symmetric=False)
-    mu_VH_unc.set_values_from_intervals(zip([2.52,2.20,1.26,1.22,0.75],[2.23,1.87,1.18,1.10,0.71]),
-                                         nominal=mu_VH.values)
+    mu_VH_unc.set_values_from_intervals(zip(mu_VH_low, mu_VH_high), nominal=mu_VH.values)
+
     mu_VH.add_uncertainty(mu_VH_unc)
     table.add_variable(mu_VH)
 
+    # ---------------- mu_VZ ----------------
     mu_VZ = Variable("mu_VZ", is_independent=False, is_binned=False, units="")
     mu_VZ.values = [-0.15, -0.83, 1.20, -0.40, 0.09]
+
+    mu_VZ_err = [1.77, 1.53, 1.28, 0.92, 0.63]  # symmetric
+
+    mu_VZ_low  = [m - e for m, e in zip(mu_VZ.values, mu_VZ_err)]
+    mu_VZ_high = [m + e for m, e in zip(mu_VZ.values, mu_VZ_err)]
+
     mu_VZ_unc = Uncertainty("mu_VZ uncertainty", is_symmetric=False)
-    
-    mu_VZ_unc.set_values_from_intervals(zip([1.77, 1.53, 1.28, 0.92, 0.63], 
-                                            [1.77, 1.53, 1.28, 0.92, 0.63]),nominal=mu_VZ.values)
+    mu_VZ_unc.set_values_from_intervals(zip(mu_VZ_low, mu_VZ_high), nominal=mu_VZ.values)
+
     mu_VZ.add_uncertainty(mu_VZ_unc)
     table.add_variable(mu_VZ)
 
